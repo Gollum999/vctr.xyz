241da50609754b0c444e66aadcfad7d3d425aa6f
Tim Sanders <tsanders@tradebot.com>
2018-11-22 17:10:12 -0600

    Initial commit

---
+++ src/components/VecViz.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello world</h1>
+</template>
+
+<script>
+export default {
+  name: 'VecViz',
+}
+</script>
+
+<style>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import VecViz from '@/components/VecViz'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+    },
+    {
+      path: '/vecviz',
+      name: 'VecViz',
+      component: VecViz,
+    },
+  ],
+})
