bcae33631dd5a72dfc2e26fe7e1784672e5e721a
Tim Sanders <tsanders@tradebot.com>
2018-11-26 00:40:27 -0600

    Add global CSS to override Rete style

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './node-editor.css';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
