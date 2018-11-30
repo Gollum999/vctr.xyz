029b09960e468dcf921370bfb218c201c7e14d68
Tim Sanders <gollum9999@gmail.com>
2018-11-29 23:50:30 -0600

    Move CSS to relevant component

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import router from './router';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import './node-editor.css';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
