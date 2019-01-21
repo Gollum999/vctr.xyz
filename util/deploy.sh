8755b95d9e858555fb13735c7063755ca1862163
Tim Sanders <gollum9999@gmail.com>
2019-01-20 19:42:22 -0600

    Update deploy script to run ssh-agent and add key if not running

--- util/deploy.sh
+++ util/deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # read -s -p"Enter passphrase for key '/home/tim/.ssh/id_rsa': " pw
-pushd $(dirname "$0")/..
-npm run build
-pushd ./dist
-git add --all
-git commit --amend -m "Initial commit"
+if ! ssh-add -l | grep -q SHA256:LGcq6VKRiXu7C88nIQxDd/DO7TQ0D4wwqAozK+IIkME; then
+    echo "Could not find ssh key, adding"
+    eval `ssh-agent -s`
+    ssh-add ~/.ssh/id_rsa
+fi
+pushd $(dirname "$0")/.. &&
+npm run build &&
+pushd ./dist &&
+git add --all &&
+git commit --amend -m "Initial commit" &&
 # export GIT_ASKPASS="echo $pw"
-git push --force
-popd
+git push --force &&
+popd &&
 popd
