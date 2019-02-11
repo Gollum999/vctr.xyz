d1d56d629eaf8e57a6fae5fbf25df5544dee248e
Tim Sanders <gollum9999@gmail.com>
2019-02-10 21:37:12 -0600

    Explicitly hide socket hints since they otherwise default to 'undefined'

--- src/node_editor/sockets.js
+++ src/node_editor/sockets.js
@@ -1,6 +1,12 @@
 import Rete from 'rete';
 
-export default {
+const sockets = {
     scalar: new Rete.Socket('Scalar value'),
     vector: new Rete.Socket('Vector value'),
 };
+
+// These are displayed when hovering over a socket of each type
+sockets.scalar.hint = '';
+sockets.vector.hint = '';
+
+export default sockets;
