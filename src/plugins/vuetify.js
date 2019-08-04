9ccc4f338fff927c18c02a71825f60260833f1ad
Tim Sanders <gollum9999@gmail.com>
2019-08-04 00:08:43 -0500

    Add vue-svg-loader, load custom SVGs as components and register them with Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
+import ScalarIcon from '@/assets/scalar.svg';
+import VectorIcon from '@/assets/vector.svg';
+import MatrixIcon from '@/assets/matrix.svg';
+import OperationIcon from '@/assets/operation.svg';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
     icons: {
         iconfont: 'mdi',
+        values: {
+            'scalar': { component: ScalarIcon },
+            'vector': { component: VectorIcon },
+            'matrix': { component: MatrixIcon },
+            'operation': { component: OperationIcon },
+        },
     },
 });
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    runtimeCompiler: true
+    runtimeCompiler: true,
+    chainWebpack: (config) => {
+        // vue-svg-loader config -- allow using svg files as Vue components
+        const svgRule = config.module.rule('svg');
+        svgRule.uses.clear();
+        svgRule
+            .use('vue-svg-loader')
+            .loader('vue-svg-loader');
+    },
 }
