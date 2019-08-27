200aa7138ed33edbb7d349f9eb5541c6f72389d7
Tim Sanders <gollum9999@gmail.com>
2019-08-26 21:18:26 -0500

    Remove packages that were replaced with Vuetify!
    bootstrap-vue
    vue-color
    vue-context-menu
    vue-js-modal
    vue-material

--- src/main.js
+++ src/main.js
@@ -1,28 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import BootstrapVue from 'bootstrap-vue';
 import VueSplit from 'vue-split-panel';
 import App from './App';
 import router from './router';
 import * as VueGL from 'vue-gl';
-import VueMaterial from 'vue-material';
-import VModal from 'vue-js-modal';
 import vuetify from './plugins/vuetify';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import 'vue-material/dist/vue-material.min.css';
-import 'vue-material/dist/theme/default.css';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 import '../static/global.css';
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue);
 Vue.use(VueSplit);
-Vue.use(VueMaterial);
-Vue.use(VModal);
 
 console.log('Initializing VueGL components');
 Object.keys(VueGL).forEach(name => {
