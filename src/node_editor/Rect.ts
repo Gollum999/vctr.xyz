e8f5ed8fb4ba351b1d151fc10d22f9b8beb7d507
Tim Sanders <tsanders@tradebot.com>
2020-06-21 18:07:44 -0500

    Convert Rect.js to TypeScript

--- src/node_editor/Rect.ts
+++ src/node_editor/Rect.ts
@@ -1,32 +1,44 @@
 // TODO is there a library for this?
+export interface SimpleRect {
+    top: number,
+    left: number,
+    right: number,
+    bottom: number,
+};
+
 export default class Rect {
-    constructor({top, left, right, bottom}) {
+    public top: number;
+    public left: number;
+    public right: number;
+    public bottom: number;
+
+    constructor({top, left, right, bottom}: SimpleRect) {
         this.top = top;
         this.left = left;
         this.right = right;
         this.bottom = bottom;
     }
 
-    width() {
+    width(): number {
         return this.right - this.left;
     }
 
-    height() {
+    height(): number {
         return this.bottom - this.top;
     }
 
-    midpoint() {
+    midpoint(): [number, number] {
         return [this.left + (this.width() / 2), this.top + (this.height() / 2)];
     }
 
-    grow(padding) {
+    grow(padding: number): void {
         this.top -= padding;
         this.bottom += padding;
         this.left -= padding;
         this.right += padding;
     }
 
-    union(other) {
+    union(other: Rect): Rect {
         return new Rect({
             top: Math.min(this.top, other.top),
             left: Math.min(this.left, other.left),
