4f1bd90083735183d25ab0d95a29eafc43780720
Tim Sanders <tsanders@tradebot.com>
2020-06-21 22:52:29 -0500

    Convert AxesLabelControlView.vue to TypeScript

--- src/node_editor/AxesLabelControlView.vue
+++ src/node_editor/AxesLabelControlView.vue
@@ -1,23 +1,25 @@
 <template>
 <div class="dimensions-label-container" :style="{'grid-row': rowIdx}">
-  <div class="label-column" v-for="axis in axes" :key="`label-column-${axis}`">{{axis | upper}}</div>
+  <div class="label-column" v-for="axis in axes" :key="`label-column-${axis}`">{{axis.toUpperCase()}}</div>
 </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import type { Emitter } from 'rete/types/core/emitter';
+import type { EventsTypes as DefaultEvents } from 'rete/types/events';
+import type { EventsTypes as CoreEvents } from 'rete/types/core/events';
+
+export default Vue.extend({
     props: {
-        getData:  { type: Function, required: true }, // injected by Rete
-        putData:  { type: Function, required: true }, // injected by Rete
-        emitter:  { type: Object,   required: true }, // injected by Rete
-        dataKey:  { type: String,   required: true }, // injected by Rete
-        rowIdx:   { type: Number,   required: true }, // used to position control within parent grid
-        axes:     { type: Array,    required: true }, // list of labels to display
+        getData: { type: Function as PropType<(key: string) => any>,              required: true }, // injected by Rete
+        putData: { type: Function as PropType<(key: string, val: any) => void>,   required: true }, // injected by Rete
+        emitter: { type: Object as PropType<Emitter<DefaultEvents & CoreEvents>>, required: true },
+        dataKey: { type: String,                                                  required: true },
+        rowIdx:  { type: Number,                                                  required: true }, // used to position control within parent grid
+        axes:    { type: Array as PropType<Array<string>>,                        required: true }, // list of labels to display
     },
-    filters: {
-        upper: String.toUpperCase,
-    },
-};
+});
 </script>
 
 <style scoped>
