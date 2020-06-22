baa8a5baca11891a798252d1a8936f3b23a7c1db
Tim Sanders <tsanders@tradebot.com>
2020-06-21 22:52:44 -0500

    Convert AxesLabelControl.js to TypeScript

--- src/node_editor/AxesLabelControl.ts
+++ src/node_editor/AxesLabelControl.ts
@@ -1,13 +1,19 @@
 import Rete from 'rete';
+import Vue from 'vue';
 import AxesLabelControlView from './AxesLabelControlView.vue';
 import { ValueNodeType } from './node_util';
+import type { Emitter } from 'rete/types/core/emitter';
+import type { EventsTypes as DefaultEvents } from 'rete/types/events';
+import type { EventsTypes as CoreEvents } from 'rete/types/core/events';
 
 export class AxesLabelControl extends Rete.Control {
-    constructor(nodeType, emitter, key, rowIdx) {
-        super(key);
-        this.render = 'vue';
-        this.component = AxesLabelControlView;
+    private readonly render = 'vue';
+    private readonly component = AxesLabelControlView;
+    private readonly props: object;
+    private readonly vueContext: InstanceType<typeof AxesLabelControlView> | null = null;
 
+    constructor(nodeType: ValueNodeType, emitter: Emitter<DefaultEvents & CoreEvents>, key: string, rowIdx: number) {
+        super(key);
         if (nodeType === ValueNodeType.VECTOR) {
             this.props = { emitter, dataKey: key, rowIdx, axes: ['X', 'Y', 'Z'] };
         } else if (nodeType === ValueNodeType.MATRIX) {
