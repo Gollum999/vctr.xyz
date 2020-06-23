127074075f115ae5e62d9163424ca33a982978da
Tim Sanders <tsanders@tradebot.com>
2020-06-22 21:36:49 -0500

    Convert ColorControl.js to TypeScript

---
+++ src/node_editor/ColorControl.ts
@@ -0,0 +1,26 @@
+import Rete from 'rete';
+import ColorControlView from './ColorControlView.vue';
+import type { Emitter } from 'rete/types/core/emitter';
+import type { EventsTypes as DefaultEvents } from 'rete/types/events';
+import type { EventsTypes as CoreEvents } from 'rete/types/core/events';
+
+export class ColorControl extends Rete.Control {
+    private readonly render = 'vue';
+    private readonly component = ColorControlView;
+    private readonly props: object;
+    private readonly vueContext: InstanceType<typeof ColorControlView> | null = null;
+
+    constructor(emitter: Emitter<DefaultEvents & CoreEvents>, key: string, rowIdx: number) {
+        super(key);
+        // console.log(`ColorControl constrtuctor, key: ${key}, row idx: ${rowIdx}`, this, this._vue, this.vueContext);
+        this.props = { emitter, dataKey: key, rowIdx };
+    }
+
+    setValue(val: string) {
+        if (this.vueContext == null) {
+            throw new Error('ColorControl Vue context was null');
+        }
+        // console.log(`ColorControl setValue to ${val}`);
+        this.vueContext.color = val;
+    }
+};
