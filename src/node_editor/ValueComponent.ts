7fba293527e1de377f767ca4d5d0423ee0ec33f6
Tim Sanders <tsanders@tradebot.com>
2020-06-28 22:52:29 -0500

    Convert ValueComponent.js to TypeScript

--- src/node_editor/ValueComponent.ts
+++ src/node_editor/ValueComponent.ts
@@ -5,26 +5,30 @@ import { ValueControl } from './ValueControl';
 import { AxesLabelControl } from './AxesLabelControl';
 import { ColorControl } from './ColorControl';
 import * as nodeUtil from './node_util';
+import type { Node } from 'rete/types/node';
+import type { Socket } from 'rete/types/socket';
+import type { Inputs as DataInputs, Outputs as DataOutputs, Node as DataNode } from 'rete/types/core/data';
 
 export class ValueComponent extends Rete.Component {
-    constructor(nodeType) {
+    constructor(private readonly nodeType: nodeUtil.ValueNodeType) {
         // TODO should consider patching Rete to allow specifying extra attributes or something
         // TODO   not sure of the best design since render plugin is entirely separate from components
         super(nodeType); // Note that the node name affects the element class as well as the node title
         this.nodeType = nodeType;
     }
 
-    builder(node) {
+    async builder(node: Node): Promise<Node> {
         // console.log('ValueComponent builder: this: ', this, 'nodeType:', this.nodeType);
 
-        const socket = {
+        const socket = ({
             [nodeUtil.ValueNodeType.SCALAR]: sockets.scalar,
             [nodeUtil.ValueNodeType.VECTOR]: sockets.vector,
             [nodeUtil.ValueNodeType.MATRIX]: sockets.matrix,
-        }[this.nodeType];
+        } as {[key: string]: Socket})[this.nodeType];
 
         node.addInput(new Rete.Input('value', 'Value', socket));
-        node.addInput(new Rete.Input('color_label', 'Color', null));
+        // rete-vue-render-plugin is the only thing that relies on socket being non-null, and I have my own NodeRenderer
+        node.addInput(new Rete.Input('color_label', 'Color', (null as any)));
         // If this key changes, NodeRenderer must also change // TODO remove this assumption
         node.addInput(new Rete.Input('pos', 'Position', sockets.vector));
 
@@ -40,7 +44,7 @@ export class ValueComponent extends Rete.Component {
         return node;
     }
 
-    worker(node, inputs, outputs) {
+    worker(node: DataNode, inputs: DataInputs, outputs: DataOutputs): void {
         // NOTE TO SELF:
         //   'node', 'inputs', and 'outputs' here are all in data-land, and are not directly tied to the nodes, inputs, and outputs in the editor
         //   'node' is the JSON descriptor for the currently processed node, suitable for serialization
@@ -52,19 +56,27 @@ export class ValueComponent extends Rete.Component {
 
         const editorNode = nodeUtil.getEditorNode(this.editor, node);
         // console.log('VECTOR', node, editorNode, inputs, editorNode.inputs);
+        const valueControl = editorNode.controls.get('value') as ValueControl;
+        if (_.isNil(valueControl)) {
+            throw new Error('Could not find "value" control');
+        }
 
         if (nodeUtil.hasInput(inputs, 'value')) {
             const inputValue = nodeUtil.getInputValue('value', inputs, node.data);
             // console.log('ValueComponent worker setting "value" from input to ', inputValue, typeof inputValue);
+
             node.data.value = inputValue.slice(); // Make a copy to avoid sharing the same object between nodes
-            editorNode.controls.get('value').setValue(inputValue);
-            editorNode.controls.get('value').setReadOnly(true);
+            valueControl.setValue(inputValue);
+            valueControl.setReadOnly(true);
         } else {
             // console.log('ValueComponent worker, inputValue empty, setting "value" readonly = false');
-            editorNode.controls.get('value').setReadOnly(false);
+            valueControl.setReadOnly(false);
         }
 
-        const posControl = editorNode.controls.get('pos');
+        const posControl = editorNode.controls.get('pos') as ValueControl;
+        if (_.isNil(posControl)) {
+            throw new Error('Could not find "pos" control');
+        }
         if (!_.isNil(posControl.vueContext)) { // Bit of a hack, could hold a reference to the global settings
             if (nodeUtil.hasInput(inputs, 'pos')) {
                 const inputPos = nodeUtil.getInputValue('pos', inputs, node.data);
