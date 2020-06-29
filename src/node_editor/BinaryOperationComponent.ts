59584bf14753dbd8b62602a141b5356e601f5109
Tim Sanders <tsanders@tradebot.com>
2020-06-28 19:04:35 -0500

    Convert BinaryOperationComponent.js to TypeScript

--- src/node_editor/BinaryOperationComponent.ts
+++ src/node_editor/BinaryOperationComponent.ts
@@ -3,26 +3,34 @@ import Rete from 'rete';
 
 import NodeRenderer from './NodeRenderer.vue';
 import * as nodeUtil from './node_util';
-import { CalculationError } from './WarningControl';
+import { CalculationError, WarningControl } from './WarningControl';
 import history from '../history';
 import { MultiAction, RemoveAllNodeOutputConnectionsAction } from '../history_actions';
+import type { BinaryOperation } from './BinaryOperation';
+import type { Node } from 'rete/types/node';
+import type { Inputs as DataInputs, Outputs as DataOutputs, Node as DataNode } from 'rete/types/core/data';
 
 export default class BinaryOperationComponent extends Rete.Component {
-    constructor(operation) {
+    private readonly operation: typeof BinaryOperation;
+
+    constructor(operation: typeof BinaryOperation) {
         // Note that the Rete engine does some node lookups by name, so each node type must have a unique name.
         // This is especially important in here where I give the component some state; if the name is shared anywhere it will look up
         //   the wrong worker() function during engine processing
+        if (operation.title == null) {
+            throw new Error(`Operation title was null: ${operation}`);
+        }
         super(operation.title);
         this.operation = operation;
         this.data.component = NodeRenderer;
     }
 
-    builder(node) {
+    async builder(node: Node): Promise<Node> {
         this.operation.setupSockets(this.editor, node);
         return node;
     }
 
-    worker(engineNode, inputs, outputs) {
+    worker(engineNode: DataNode, inputs: DataInputs, outputs: DataOutputs): void {
         const lhsValue = nodeUtil.getInputValue('lhs', inputs, engineNode.data);
         const rhsValue = nodeUtil.getInputValue('rhs', inputs, engineNode.data);
         if (_.isNil(lhsValue) || _.isNil(rhsValue)) {
@@ -31,7 +39,7 @@ export default class BinaryOperationComponent extends Rete.Component {
 
         // Bit of a hack; probably more correct to put something in node.data, or maybe  look at the type of the node on the other
         // side of the input connection
-        function determineType(val) {
+        function determineType(val: Array<number>): string {
             switch (val.length) {
             case 1:  return 'scalar';
             case 3:  return 'vector';
@@ -42,15 +50,22 @@ export default class BinaryOperationComponent extends Rete.Component {
 
         // console.log(lhsValue, rhsValue);
         const editorNode = nodeUtil.getEditorNode(this.editor, engineNode);
+        const warningControl = editorNode.controls.get('warning') as WarningControl;
         let result;
         try {
-            result = this.operation.calculate({type: determineType(lhsValue), value: lhsValue}, {type: determineType(rhsValue), value: rhsValue});
+            result = this.operation.calculate(
+                {type: determineType(lhsValue), value: lhsValue},
+                {type: determineType(rhsValue), value: rhsValue},
+            );
             editorNode.data['disabled'] = false;
-            editorNode.controls.get('warning').setWarning('');
+            if (warningControl == null) {
+                throw new Error('WarningControl was null');
+            }
+            warningControl.setWarning('');
         } catch (e) {
             if (e instanceof CalculationError) {
                 editorNode.data['disabled'] = true;
-                editorNode.controls.get('warning').setWarning(e.message);
+                warningControl.setWarning(e.message);
                 if (Array.from(editorNode.outputs.values()).some(io => io.connections.length)) {
                     const action = new RemoveAllNodeOutputConnectionsAction(this.editor, editorNode);
                     history.addAndDo(action);
