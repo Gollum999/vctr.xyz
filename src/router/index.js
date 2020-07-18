89630e3dede029f68c208b525515d14ced657f09
Tim Sanders <tsanders@tradebot.com>
2020-07-18 18:21:31 -0500

    Delete HelloWorld component and related tests/routes
    (How has this survived this long?)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/HelloWorld';
 import VecViz from '@/VecViz';
 
 Vue.use(Router);
@@ -9,11 +8,6 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld,
-        },
-        {
-            path: '/vecviz',
             name: 'VecViz',
             component: VecViz,
         },
