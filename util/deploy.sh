568f32b5ddd4006d44851d7f662741c08d311b59
Gollum999 <gollum9999@gmail.com>
2020-07-26 23:39:43 -0500

    Change deploy script to the one recommended by Vue

--- util/deploy.sh
+++ util/deploy.sh
@@ -1,20 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-# read -s -p"Enter passphrase for key '/home/tim/.ssh/id_rsa': " pw
-if ! ssh-add -l | grep -q SHA256:LGcq6VKRiXu7C88nIQxDd/DO7TQ0D4wwqAozK+IIkME; then
-    echo "Could not find ssh key, adding"
-    eval `ssh-agent -s`
-    ssh-add ~/.ssh/id_rsa
-fi
-pushd $(dirname "$0")/.. &&
-npm run build &&
-pushd ./dist &&
-git init &&
-git remote add origin "git@bitbucket.org:Gollum999/gollum999.bitbucket.io.git" &&
-git fetch origin &&
-git add --all &&
-git commit -m "Initial commit" &&
-# export GIT_ASKPASS="echo $pw"
-git push --force --set-upstream origin master &&
-popd &&
-popd
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:Gollum999/vctr.xyz.git master:gh-pages
+
+cd -
